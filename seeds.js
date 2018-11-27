d2d0dd182450c8225053e558cbd376d1d31074f3
Ann <anntuck@anntuck.com>
2018-11-26 23:22:06 -0500

    Added seed file

---
+++ seeds.js
@@ -0,0 +1,9 @@
+use kudos_db
+
+db.kudos.insert({"title": "Hello", "body":"How are you?", "from": "Tina Turner", "to": "Johnny Cash"})
+
+db.user.insert({"username": "Tina Turner"})
+db.user.insert({"username": "Amy Winehouse"})
+db.user.insert({"username": "Johnny Cash"})
+db.user.insert({"username": "Engelbert Humperdink"})
+
