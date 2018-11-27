9ac1a7b801754afae10a9cd751697419b013f8d3
Ann <anntuck@anntuck.com>
2018-11-27 00:10:58 -0500

    Modified server.js for Heroku deploy with mLab

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+//For Deploy start
+// var request = require('request');
+// var cheerio = require('cheerio');
+//For Deploy end
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -12,7 +16,23 @@ app.use(express.static("public"));
 
 mongoose.connect('mongodb://localhost/kudos_db', { useNewUrlParser: true });
 
+//Stuff from MongoDBMLabHerokuDeploymentProcess.pdf  Start
 
+var databaseUri = 'mongodb://localhost/week18day3mongoose';
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);  
+} else {
+  mongoose.connect(databaseUri);
+}
+var db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+//Stuff from MongoDBMLabHerokuDeploymentProcess.pdf  End
 require('./routes/api-routes')(app);
 
 
